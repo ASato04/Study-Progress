ca9767cafeac9338a1adc931cfca0c87abcf60ab
ASato04 <a.sato2510@outlook.com>
2024-10-11 14:53:42 -0300

    Create Product Registration.kt
    Created an array of product/price pairs input via readln

---
+++ Product Registration.kt
@@ -0,0 +1,24 @@
+var registrationBase:Array<Pair<String,Float>> = arrayOf()
+
+
+fun inputProducts (registration:Array<Pair<String,Float>>):Array<Pair<String,Float>> {
+    println("'stop' for exit")
+    val name = readln()
+    return if (name == "stop") {
+        registration
+        }
+
+    else{
+    val price = readln().toFloat()
+    val productAndPrice = Pair(name, price)
+
+    inputProducts(registration + productAndPrice)
+    }}
+
+
+
+fun main (){
+    val x:Array<Pair<String,Float>> =(inputProducts(registrationBase))
+    x.forEach { (product, price) ->
+        println("Produto: $product, Preço: $price")
+    }}
